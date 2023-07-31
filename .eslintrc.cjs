3d8a8c98c971dfd53b3d4cbb5fd60e968646fd4b
a-ryang <chnwu.kim@gmail.com>
2023-08-01 01:11:54 +0900

    chore: lint 추가 설정 #1
    * eslint-config-prettier : ESlint, Prettier 충돌 방지
    
    * eslint-import-resolver-typescript : eslint-plugin-import에 TS 지원
    
    * eslint-plugin-import : JS import/export 문법 오류, 이름 오타 방지
    
    * eslint-plugin-jest-dom : Jest DOM 테스팅에 Lint 제공
    
    * eslint-plugin-prettier : ESLint, Prettier 통합
    
    * eslint-plugin-react : React, JSX에 Lint 제공
    
    * eslint-plugin-react-hooks : React Hooks에 Lint 제공
    
    * eslint-plugin-react-refresh : React fast refresh 지원
    
    * eslint-plugin-testing-library : testing library에 Lint 제공

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,47 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:prettier/recommended",
+    "plugin:testing-library/react",
+    "plugin:jest-dom/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", "node_modules/*"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react/react-in-jsx-scope": "off",
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin", // nodejs module
+          "external", // node_modules
+          "internal", // in package
+          "parent", // parent dir
+          "sibling", // same dir
+          "index", // same dir and index file
+          "object", // json
+        ],
+        "newlines-between": "always",
+        alphabetize: { order: "asc", caseInsensitive: true },
+      },
+    ],
   },
-}
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from "vite";
 import { fileURLToPath, URL } from "node:url";
+
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
