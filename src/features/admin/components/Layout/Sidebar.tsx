6dc06723cc6bf29906ffb1a79a8aef58ccbb408d
a-ryang <chnwu.kim@gmail.com>
2023-08-08 16:39:34 +0900

    style: 반대로 된 css 값 수정

--- src/features/admin/components/Layout/Sidebar.tsx
+++ src/features/admin/components/Layout/Sidebar.tsx
@@ -22,7 +22,7 @@ const Header = styled.div<{ isCollapsed: boolean }>(
     },
   },
   ({ isCollapsed }) => ({
-    paddingLeft: isCollapsed ? ".5rem" : "",
+    paddingLeft: isCollapsed ? "" : ".5rem",
   }),
 );
 
