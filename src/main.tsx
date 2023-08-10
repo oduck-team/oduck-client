7131456e3857ece4f73edd89720b82cdf2fcfec9
a-ryang <chnwu.kim@gmail.com>
2023-08-10 19:35:17 +0900

    refactor: reset, font css #3
    * runtime css 아닌 일반 css로 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
+
+import "./assets/style.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
