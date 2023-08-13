efdcd3f9af624745395649465fb02d25d8d1cce6
a-ryang <chnwu.kim@gmail.com>
2023-08-13 17:22:43 +0900

    feat: Backdrop에 onClick 추가 #15

--- src/components/Backdrop/index.tsx
+++ src/components/Backdrop/index.tsx
@@ -5,15 +5,17 @@ import { Container } from "./style";
 export interface BackdropProps {
   readonly isVisible?: boolean;
   readonly className?: string;
+  readonly onClick: () => void;
 }
 
 export default function Backdrop({
   isVisible = true,
   className = "",
+  onClick,
   children,
 }: StrictPropsWithChildren<BackdropProps>) {
   return (
-    <Container isVisible={isVisible} className={className}>
+    <Container isVisible={isVisible} className={className} onClick={onClick}>
       {children}
     </Container>
   );
