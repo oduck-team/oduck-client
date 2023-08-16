4edddaf69d6ee1cb28269446e7f438f03499a0d2
a-ryang <chnwu.kim@gmail.com>
2023-08-16 21:19:13 +0900

    refactor: breakPoints as const 처리

--- src/styles/mediaQuery.ts
+++ src/styles/mediaQuery.ts
@@ -1,12 +1,12 @@
-const breakPoints = {
+export type MediaQuery = typeof mq;
+
+export const breakPoints = {
   xs: 420,
   sm: 576,
   md: 768,
   lg: 900,
   xl: 1200,
-};
+} as const;
 
 export const mq = (bp: keyof typeof breakPoints) =>
   `@media (min-width: ${breakPoints[bp]}px)`;
-
-export type MediaQuery = typeof mq;
