ab0dc17cb3784a42412455328ff5fe52051772ec
mihee <s2809h29@gmail.com>
2023-08-24 15:20:01 +0900

    feat: CardReview #41

---
+++ src/features/reviews/components/CardReview.tsx
@@ -0,0 +1,74 @@
+import { ComponentProps, useState } from "react";
+
+import Rating from "@/components/Rating";
+import { StrictPropsWithChildren } from "@/types";
+
+import {
+  ActionsContainer,
+  AnimationContainer,
+  Container,
+  ContentContainer,
+  Image,
+  InfoContainer,
+  SpoilerButtonContainer,
+} from "./CardReview.style";
+
+interface AniProps {
+  title: string;
+  image: string;
+  rating: number;
+}
+
+export default function CardReview({
+  children,
+  className,
+}: StrictPropsWithChildren<{ className?: string }>) {
+  return <Container className={className}>{children}</Container>;
+}
+
+CardReview.Animation = Animation;
+CardReview.Content = Content;
+CardReview.Actions = Actions;
+
+function Animation({ title, image, rating }: AniProps) {
+  return (
+    <AnimationContainer>
+      <Image image={image}></Image>
+      <InfoContainer>
+        <div>{title}</div>
+        <Rating color="primary" value={rating} size="sm" readonly />
+      </InfoContainer>
+    </AnimationContainer>
+  );
+}
+
+function Content({
+  isSpoiler = false,
+  children,
+}: StrictPropsWithChildren<{ isSpoiler?: boolean }>) {
+  const [showContent, setShowContent] = useState(!isSpoiler);
+  return (
+    <ContentContainer>
+      {isSpoiler && !showContent ? (
+        <SpoilerButton onClick={() => setShowContent(true)} />
+      ) : (
+        <>{children}</>
+      )}
+    </ContentContainer>
+  );
+}
+
+function SpoilerButton({ onClick }: { onClick: () => void }) {
+  return (
+    <SpoilerButtonContainer onClick={onClick}>
+      스포일러를 포함한 리뷰에요! <span>리뷰 보기</span>
+    </SpoilerButtonContainer>
+  );
+}
+
+function Actions({
+  children,
+  ...props
+}: StrictPropsWithChildren<ComponentProps<"div">>) {
+  return <ActionsContainer {...props}>{children}</ActionsContainer>;
+}
