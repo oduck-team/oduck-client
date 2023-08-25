b995fb9ea6e303d737fe9613b260263d7259dd98
a-ryang <chnwu.kim@gmail.com>
2023-08-25 12:36:13 +0900

    chore: 빌드를 위한 주석처리

--- src/routes/privateRoutes.tsx
+++ src/routes/privateRoutes.tsx
@@ -1,10 +1,10 @@
 import { RouteObject } from "react-router-dom";
 
-import { StrictPropsWithChildren } from "@/types";
+// import { StrictPropsWithChildren } from "@/types";
 
-function PrivateRoute({ children }: StrictPropsWithChildren) {
-  // TODO: USER ROLE 확인
-  return children;
-}
+// function PrivateRoute({ children }: StrictPropsWithChildren) {
+//   // TODO: USER ROLE 확인
+//   return children;
+// }
 
 export const privateRoutes: RouteObject[] = [];
