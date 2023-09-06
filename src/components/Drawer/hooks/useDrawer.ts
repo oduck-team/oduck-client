2f66d55aac4f91a5cdddcca5657af7dc3203c9a1
a-ryang <chnwu.kim@gmail.com>
2023-09-06 14:42:51 +0900

    feat: Drawer 터치 슬라이드 닫기 구현 #37

---
+++ src/components/Drawer/hooks/useDrawer.ts
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+export type Position = "top" | "right" | "bottom" | "left";
+
+export function useDrawer(position: Position, onClose: () => void) {
+  const [startX, setStartX] = useState(0);
+  const [currentX, setCurrentX] = useState(0);
+  const [startY, setStartY] = useState(0);
+  const [currentY, setCurrentY] = useState(0);
+
+  const handleTouchStart = (e: React.TouchEvent) => {
+    setStartX(e.touches[0].clientX);
+    setStartY(e.touches[0].clientY);
+  };
+
+  const handleTouchMove = (e: React.TouchEvent) => {
+    setCurrentX(e.touches[0].clientX);
+    setCurrentY(e.touches[0].clientY);
+  };
+
+  /**
+   * @description 사용자의 터치 방향으로 Drawer 닫기 여부를 정합니다.
+   */
+  const handleTouchEnd = () => {
+    const diffX = currentX - startX;
+    const diffY = currentY - startY;
+
+    let shouldClose = false;
+
+    switch (position) {
+      case "right":
+        shouldClose = diffX > 50;
+        break;
+      case "left":
+        shouldClose = diffX < -50;
+        break;
+      case "top":
+        shouldClose = diffY < -50;
+        break;
+      case "bottom":
+        shouldClose = diffY > 50;
+        break;
+      default:
+        break;
+    }
+
+    if (shouldClose) {
+      onClose();
+    }
+
+    setStartX(0);
+    setCurrentX(0);
+    setStartY(0);
+    setCurrentY(0);
+  };
+
+  return { handleTouchStart, handleTouchMove, handleTouchEnd };
+}
