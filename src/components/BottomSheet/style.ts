50bcac5349d0f58d8a9741901d6123e320571eb7
a-ryang <chnwu.kim@gmail.com>
2023-09-08 12:28:26 +0900

    feat: BottomSheet 핸들바 구현 #37

--- src/components/BottomSheet/style.ts
+++ src/components/BottomSheet/style.ts
@@ -4,59 +4,80 @@ import { motion } from "framer-motion";
 import BaseBackdrop from "../Backdrop";
 
 export const Backdrop = styled(BaseBackdrop)`
+  justify-content: center;
   z-index: ${({ theme }) => theme.zIndex.bottomSheet};
 `;
 
 export const Container = styled(motion.div)`
-  position: fixed;
+  position: relative;
+  bottom: 0;
   display: flex;
   flex-direction: column;
-  padding: 32px 24px;
-  max-height: 80%;
+  height: 100lvh;
   width: 100%;
   max-width: 600px;
-  bottom: 0;
-  background-color: ${({ theme }) => theme.colors.neutral["05"]};
-  border-radius: 30px 30px 0px 0px;
-  box-shadow: 0px -5px 16px 0px rgba(0, 0, 0, 0.09);
+  background-color: #ffffff;
+  border-radius: 20px 20px 0px 0px;
   overflow: hidden;
 `;
 
-export const Header = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  padding-bottom: 1rem;
+export const Handlebar = styled.div`
+  min-height: 30px;
+  cursor: grab;
 
-  // 각 영역의 내용을 중앙에 배치
-  .left,
-  .center,
-  .right {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-
-  // 왼쪽 영역의 내용을 왼쪽에 배치
-  .left {
-    justify-content: flex-start;
-  }
-
-  // 오른쪽 영역의 내용을 오른쪽에 배치
-  .right {
-    justify-content: flex-end;
+  &::before {
+    position: absolute;
+    top: 14px;
+    left: 50%;
+    transform: translateX(-50%);
+    content: "";
+    display: block;
+    height: 4px;
+    width: 40px;
+    border-radius: 5px;
+    background-color: ${({ theme }) => theme.colors.neutral["30"]};
+    box-shadow: inset 0px 0.5px 0.1px rgba(0, 0, 0, 0.06);
   }
 `;
 
-export const Content = styled.div`
+export const ContentContainer = styled.div`
+  height: fit-content;
   overflow-y: auto;
   -ms-overflow-style: none;
   scrollbar-width: none;
-
   &::-webkit-scrollbar {
     display: none;
   }
 `;
 
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0 24px;
+  padding-bottom: 250px; // mobile
+
+  ${({ theme }) => theme.mq("sm")} {
+    padding-bottom: 50px;
+  }
+`;
+
 export const Footer = styled.div`
-  margin-top: 1rem;
+  position: fixed;
+  bottom: 0;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 24px;
+  padding-top: 14px;
+  gap: 40px;
+  min-height: fit-content;
+  width: 100%;
+  max-width: 600px;
+  background-color: #ffffff;
+  border-top: solid 1px ${({ theme }) => theme.colors.neutral["10"]};
+  z-index: 1000;
+
+  button {
+    flex-shrink: 0;
+  }
 `;
