0ce79fbe409ce0c38c6b7f5306bd1b02e5b03188
a-ryang <chnwu.kim@gmail.com>
2023-09-11 13:10:41 +0900

    feat: useFetch 커스텀 훅

---
+++ src/hooks/useFetch.tsx
@@ -0,0 +1,113 @@
+import { useEffect, useReducer } from "react";
+
+import { ApiError } from "@/lib/error";
+
+interface State<T> {
+  data?: T;
+  error?: Error;
+  isLoading: boolean;
+  isFetched: boolean;
+  isError: boolean;
+}
+
+type Action<T> =
+  | { type: "loading" }
+  | { type: "fetched"; payload: T }
+  | { type: "error"; payload: Error };
+
+const BASE_URL = import.meta.env.VITE_BASE_URL as string;
+
+// fetch API RequestInit의 method가 string이므로 오타 방지를 위한 확장 인터페이스
+export interface RequestOptions extends RequestInit {
+  method: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+}
+
+export default function useFetch<T>(url: string, options?: RequestOptions) {
+  const initialState: State<T> = {
+    data: undefined,
+    error: undefined,
+    isLoading: false,
+    isFetched: false,
+    isError: false,
+  };
+
+  const fetchReducer = (state: State<T>, action: Action<T>): State<T> => {
+    switch (action.type) {
+      case "loading":
+        return {
+          ...state,
+          isLoading: true,
+          isFetched: false,
+          isError: false,
+        };
+      case "fetched":
+        return {
+          ...state,
+          data: action.payload,
+          isLoading: false,
+          isFetched: true,
+          isError: false,
+        };
+      case "error":
+        return {
+          ...state,
+          error: action.payload,
+          isLoading: false,
+          isFetched: false,
+          isError: true,
+        };
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(fetchReducer, initialState);
+
+  useEffect(() => {
+    const fetchData = async (url: string, options?: RequestOptions) => {
+      dispatch({ type: "loading" });
+
+      try {
+        const response = await fetch(
+          url,
+          getRequestParams(options ?? { method: "GET" }),
+        );
+
+        const data = await response.json();
+
+        if (response.status >= 500) {
+          throw new Error(response.statusText);
+        }
+
+        if (response.status >= 400) {
+          throw new ApiError(data.message ?? response.statusText);
+        }
+
+        dispatch({ type: "fetched", payload: data as T });
+      } catch (error) {
+        dispatch({ type: "error", payload: error as Error });
+      }
+    };
+    fetchData(BASE_URL + url, options);
+  }, [url, options]);
+
+  return state;
+}
+
+export function getRequestParams(options: RequestOptions) {
+  const params: RequestOptions = {
+    ...options,
+    credentials: "include",
+  };
+
+  if (options.method === "GET" || options.method === "DELETE") {
+    return params;
+  }
+
+  params.headers = {
+    "Content-Type": "application/json; charset=UTF-8",
+    ...params.headers,
+  };
+
+  return params;
+}
