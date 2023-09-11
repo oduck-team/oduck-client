89230a326db655b35af8c018ad46efda5a4a8468
a-ryang <chnwu.kim@gmail.com>
2023-09-11 15:59:19 +0900

    style: Badge props 주석

--- src/components/Badge/index.tsx
+++ src/components/Badge/index.tsx
@@ -8,10 +8,17 @@ type BadgeStyle = "fill" | "inverted";
 type BadgeColor = keyof typeof theme.colors;
 
 export interface BadgeProps extends ComponentProps<"span"> {
+  /**
+   * true일 경우 단순한 점 뱃지 컴포넌트가 됩니다
+   */
   isDot?: boolean;
   styleType?: BadgeStyle;
   color?: BadgeColor;
   count?: number;
+  /**
+   * count가 오버플로우로 처리되는 수입니다
+   * @default 99
+   */
   overflowCount?: number;
 }
 
