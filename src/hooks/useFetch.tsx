6e62693622ffdfc4c6b1aacc4a23f818297f51b3
a-ryang <chnwu.kim@gmail.com>
2023-09-12 21:41:47 +0900

    refactor: env BASE_URL config에 추가

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useReducer } from "react";
 
+import { BASE_URL } from "@/config";
 import { ApiError } from "@/lib/error";
 
 interface State<T> {
@@ -14,8 +15,6 @@ type Action<T> =
   | { type: "loading" }
   | { type: "fetched"; payload: T }
   | { type: "error"; payload: Error };
-
-const BASE_URL = import.meta.env.VITE_BASE_URL as string;
 
 // fetch API RequestInit의 method가 string이므로 오타 방지를 위한 확장 인터페이스
 export interface RequestOptions extends RequestInit {
