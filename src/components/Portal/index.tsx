c869044d18f075de2999e654a02b5ce3311d9a30
a-ryang <chnwu.kim@gmail.com>
2023-09-23 02:32:55 +0900

    refactor: protal을 웹 문서에 동적으로 붙도록 수정

--- src/components/Portal/index.tsx
+++ src/components/Portal/index.tsx
@@ -1,15 +1,24 @@
+import { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 
 import { StrictPropsWithChildren } from "@/types";
 
 interface PortalProps {
-  elementId: string;
+  elementId?: string;
 }
 
 export default function Portal({
-  elementId,
   children,
 }: StrictPropsWithChildren<PortalProps>) {
-  const portal = document.getElementById(elementId);
-  return portal && createPortal(children, portal);
+  const [container, setContainer] = useState<Element | null>(null);
+
+  useEffect(() => {
+    if (document) {
+      setContainer(document.body);
+    }
+  }, [container]);
+
+  if (!container) return null;
+
+  return createPortal(children, container);
 }
