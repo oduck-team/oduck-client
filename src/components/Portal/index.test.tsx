b6240ef624f331d9fa95587ce287dd3a38020508
a-ryang <chnwu.kim@gmail.com>
2023-09-23 10:28:35 +0900

    test: Portal, AnimatePortal

--- src/components/Portal/index.test.tsx
+++ src/components/Portal/index.test.tsx
@@ -4,33 +4,15 @@ import { describe, afterEach, it, expect } from "vitest";
 import Portal from ".";
 
 describe("components/Portal", () => {
-  function createElementWithId(id: string) {
-    const element = document.createElement("div");
-    element.id = id;
-    document.body.appendChild(element);
-  }
-
   afterEach(cleanup);
 
   it("정의되어 있어야 한다", () => {
     expect(Portal).toBeDefined();
   });
 
-  it("element id가 존재한다면 children을 렌더링한다", () => {
-    const ID = "portal";
-    createElementWithId(ID);
-
-    render(<Portal elementId={ID}>children</Portal>);
+  it("children을 렌더링한다", () => {
+    render(<Portal>children</Portal>);
 
     expect(screen.getByText("children")).toBeInTheDocument();
-  });
-
-  it("element id가 존재하지 않는다면 children을 렌더링하지 않는다", () => {
-    const ID = "portal";
-    createElementWithId(ID);
-
-    render(<Portal elementId="typo-id">children</Portal>);
-
-    expect(screen.queryByText("children")).not.toBeInTheDocument();
   });
 });
