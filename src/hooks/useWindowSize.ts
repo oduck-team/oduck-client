ee9c0897f26f02f2d3f60d2c92e645e4f45bda2a
a-ryang <chnwu.kim@gmail.com>
2023-09-24 13:38:04 +0900

    feat: useWindowSize 마운트시 상태 값 설정

--- src/hooks/useWindowSize.ts
+++ src/hooks/useWindowSize.ts
@@ -3,8 +3,8 @@ import { useEffect, useState } from "react";
 import { isBrowser } from "@/utils/common";
 
 interface State {
-  width: number;
-  height: number;
+  width: number | null;
+  height: number | null;
 }
 
 export default function useWindowSize() {
@@ -16,12 +16,19 @@ export default function useWindowSize() {
   useEffect(() => {
     if (!isBrowser()) return;
 
+    // mount시
+    setState({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+
     const resizeHandler = () => {
       setState({
         width: window.innerWidth,
         height: window.innerHeight,
       });
     };
+
     window.addEventListener("resize", resizeHandler);
 
     return () => {
