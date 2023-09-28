0397b0e4b0b8e9f75edfad7034f814b6d008ac26
presentKey <kim.hyeonjae4@gmail.com>
2023-09-28 22:17:20 +0900

    feat: head title 설정

--- src/features/users/routes/ProfileEdit/index.tsx
+++ src/features/users/routes/ProfileEdit/index.tsx
@@ -1,3 +1,5 @@
+import Head from "@/components/Head";
+
 import ProfileAvatar from "../../components/ProfileAvatar";
 import ProfileImageSection from "../../components/ProfileImageSection";
 
@@ -7,6 +9,7 @@ import { ProfileEditContainer } from "./style";
 export default function ProfileEdit() {
   return (
     <ProfileEditContainer>
+      <Head title="오덕 | 프로필 수정" />
       <ProfileImageSection>
         <ProfileImageSection.Art src="https://newsimg.sedaily.com/2023/07/16/29S5JPWPBV_1.jpeg" />
         <ProfileImageSection.ArtEditButton />
