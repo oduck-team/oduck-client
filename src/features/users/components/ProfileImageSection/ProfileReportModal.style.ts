18ffaa5b93ab2cf301b799940121ecac1dc48cce
presentKey <kim.hyeonjae4@gmail.com>
2023-10-02 14:26:04 +0900

    style: ReportSucessMessage bottom 위치 조정

--- src/features/users/components/ProfileImageSection/ProfileReportModal.style.ts
+++ src/features/users/components/ProfileImageSection/ProfileReportModal.style.ts
@@ -27,7 +27,7 @@ export const ReportSucessMessage = styled.div`
   height: 40px;
   position: fixed;
   left: 50%;
-  bottom: 80px;
+  bottom: 74px; // 66 + 8
   transform: translateX(-50%);
   background-color: ${({ theme }) => theme.colors.neutral[90]};
   color: ${({ theme }) => theme.colors.neutral["05"]};
