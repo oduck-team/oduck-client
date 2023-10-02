b20a37fc5747a6da27ebaf465300fde003c78e77
a-ryang <chnwu.kim@gmail.com>
2023-10-02 13:31:25 +0900

    refactor: 스포일러 체크박스에 체크박스 컴포넌트 반영

--- src/features/reviews/components/ReviewRating/SpoilerCheckBox.tsx
+++ src/features/reviews/components/ReviewRating/SpoilerCheckBox.tsx
@@ -1,9 +1,11 @@
+import CheckBox from "@/components/CheckBox";
+
 import { SpoilerCheckBoxContainer } from "./SpoilerCheckBox.style";
 
 export default function SpoilerCheckBox() {
   return (
     <SpoilerCheckBoxContainer>
-      todo check box 컴포넌트 개선후 반영
+      <CheckBox /> 스포일러
     </SpoilerCheckBoxContainer>
   );
 }
