03e25760e75d770c0717baee1cd2a4601438d67d
presentKey <kim.hyeonjae4@gmail.com>
2023-10-03 21:25:56 +0900

    feat: 리뷰 신고하기 퍼블리싱 #138

--- src/features/reviews/components/ReviewCard/ReviewMoreButton.tsx
+++ src/features/reviews/components/ReviewCard/ReviewMoreButton.tsx
@@ -2,18 +2,29 @@ import { useTheme } from "@emotion/react";
 import { DotsThree } from "@phosphor-icons/react";
 
 import Button from "@/components/Button";
+import SnackBar from "@/components/SnackBar";
+import useSnackBar from "@/components/SnackBar/useSnackBar";
 import DropDownModal from "@/features/users/components/DropDownModal";
 import useDropDownModal from "@/features/users/components/DropDownModal/hooks/useDropDownModal";
 
-const USER_MOCK_DATA = { isMine: true };
+const USER_MOCK_DATA = { isMine: false };
 
 export default function ReviewMoreButton() {
   const theme = useTheme();
   const { isDropDownModalOpen, handleDropDownModalToggle } = useDropDownModal();
+  const { isSnackBarOpen, openSnackBar, removeSnackBarAfter } = useSnackBar();
   const handleReviewEditClick = () => console.log("리뷰수정");
   const handleReviewDeleteClick = () => console.log("리뷰삭제");
-  const handleReviewSpoilerReport = () => console.log("스포일러 신고");
-  const handleReviewEtcReport = () => console.log("기타 신고");
+  const handleReviewSpoilerReport = () => {
+    handleDropDownModalToggle();
+    openSnackBar();
+    removeSnackBarAfter(2000);
+  };
+  const handleReviewEtcReport = () => {
+    handleDropDownModalToggle();
+    openSnackBar();
+    removeSnackBarAfter(2000);
+  };
 
   return (
     <>
@@ -56,6 +67,7 @@ export default function ReviewMoreButton() {
           {USER_MOCK_DATA.isMine ? "삭제하기" : "기타 신고"}
         </DropDownModal.Button>
       </DropDownModal>
+      {isSnackBarOpen && <SnackBar text="신고가 접수되었습니다" />}
     </>
   );
 }
