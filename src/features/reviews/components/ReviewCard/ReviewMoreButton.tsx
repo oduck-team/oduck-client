1e50e668c71129609e926c18ccc9cff3aa9b4051
presentKey <kim.hyeonjae4@gmail.com>
2023-10-03 20:53:17 +0900

    feat: 리뷰 더보기 버튼의DropDownModal 마크업

--- src/features/reviews/components/ReviewCard/ReviewMoreButton.tsx
+++ src/features/reviews/components/ReviewCard/ReviewMoreButton.tsx
@@ -2,16 +2,60 @@ import { useTheme } from "@emotion/react";
 import { DotsThree } from "@phosphor-icons/react";
 
 import Button from "@/components/Button";
+import DropDownModal from "@/features/users/components/DropDownModal";
+import useDropDownModal from "@/features/users/components/DropDownModal/hooks/useDropDownModal";
+
+const USER_MOCK_DATA = { isMine: true };
 
 export default function ReviewMoreButton() {
   const theme = useTheme();
+  const { isDropDownModalOpen, handleDropDownModalToggle } = useDropDownModal();
+  const handleReviewEditClick = () => console.log("리뷰수정");
+  const handleReviewDeleteClick = () => console.log("리뷰삭제");
+  const handleReviewSpoilerReport = () => console.log("스포일러 신고");
+  const handleReviewEtcReport = () => console.log("기타 신고");
+
   return (
-    <Button
-      name="더보기"
-      icon={<DotsThree color={theme.colors.neutral["50"]} />}
-      variant="text"
-      size="sm"
-      color="neutral"
-    ></Button>
+    <>
+      <Button
+        name="더보기"
+        icon={<DotsThree color={theme.colors.neutral["50"]} />}
+        variant="text"
+        size="sm"
+        color="neutral"
+        onClick={handleDropDownModalToggle}
+      />
+      <DropDownModal
+        isVisible={isDropDownModalOpen}
+        onDropDownModalToggle={handleDropDownModalToggle}
+      >
+        <DropDownModal.Button
+          name={USER_MOCK_DATA.isMine ? "수정하기" : "스포일러 신고"}
+          size="lg"
+          variant="solid"
+          color="neutral"
+          onClick={() =>
+            USER_MOCK_DATA.isMine
+              ? handleReviewEditClick()
+              : handleReviewSpoilerReport()
+          }
+        >
+          {USER_MOCK_DATA.isMine ? "수정하기" : "스포일러 신고"}
+        </DropDownModal.Button>
+        <DropDownModal.Button
+          name={USER_MOCK_DATA.isMine ? "삭제하기" : "기타 신고"}
+          size="lg"
+          variant="solid"
+          color="neutral"
+          onClick={() =>
+            USER_MOCK_DATA.isMine
+              ? handleReviewDeleteClick()
+              : handleReviewEtcReport()
+          }
+        >
+          {USER_MOCK_DATA.isMine ? "삭제하기" : "기타 신고"}
+        </DropDownModal.Button>
+      </DropDownModal>
+    </>
   );
 }
