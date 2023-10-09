525a79b5eaad3bffeb8fce29a1405077552359eb
presentKey <kim.hyeonjae4@gmail.com>
2023-10-09 22:40:07 +0900

    feat: 입덕애니 퍼블리싱

---
+++ src/features/users/routes/Profile/TabMenu/BookmarkList.style.ts
@@ -0,0 +1,6 @@
+import styled from "@emotion/styled";
+
+export const BookmarkListContainer = styled.div`
+  border-top: 2px solid ${({ theme }) => theme.colors.neutral["05"]};
+  padding: 16px;
+`;
--- src/features/users/routes/Profile/TabMenu/BookmarkList.tsx
+++ src/features/users/routes/Profile/TabMenu/BookmarkList.tsx
@@ -1,3 +1,30 @@
+import BookmarkCard from "./BookmarkCard";
+import { BookmarkListContainer } from "./BookmarkList.style";
+
+const BOOKMARK_RIST = [
+  {
+    title: "레벨 1이지만 유니크 스킬로 최강이 되었습니다",
+    image:
+      "https://i.namu.wiki/i/v8ca2gF_MPV_L4QZGoN449G29Nt8vy3PtSLKv1T9XwmZBJ8p1GTz3S3Y32sXB-eoGDv5npoGXzpD6fASoQFLwg.webp",
+    rating: 10,
+    myRating: 9,
+    createdAt: "2023.07.30",
+  },
+  {
+    title:
+      "아주 긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴긴 제목을 가지고 있는 애니메이션",
+    image: "https://url.kr/4gtucf",
+    rating: 3,
+    createdAt: "2023.07.29",
+  },
+];
+
 export default function BookmarkList() {
-  return <div>북마크리스트</div>;
+  return (
+    <BookmarkListContainer>
+      {BOOKMARK_RIST.map((bookmark, index) => (
+        <BookmarkCard key={index} bookmark={bookmark} />
+      ))}
+    </BookmarkListContainer>
+  );
 }
