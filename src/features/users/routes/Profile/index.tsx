7991e34d70938c3d71209fd9a608685d9223aade
presentKey <kim.hyeonjae4@gmail.com>
2023-10-11 17:36:30 +0900

    feat: Head 컴포넌트 적용

--- src/features/users/routes/Profile/index.tsx
+++ src/features/users/routes/Profile/index.tsx
@@ -1,12 +1,18 @@
+import Head from "@/components/Head";
+
 import AboutMe from "./AboutMe";
 import { ProfileContainer } from "./style";
 import TabMenu from "./TabMenu";
 
 export default function Profile() {
   return (
-    <ProfileContainer>
-      <AboutMe />
-      <TabMenu />
-    </ProfileContainer>
+    <>
+      {/* FIXME: 내 프로필 -> 내 프로필, 다른 사용자 프로필 -> 닉네임*/}
+      <Head title="오덕 | OOO" />
+      <ProfileContainer>
+        <AboutMe />
+        <TabMenu />
+      </ProfileContainer>
+    </>
   );
 }
