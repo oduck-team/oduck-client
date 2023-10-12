054fe7ba112202eecbe3ba004f8e74b9b27de960
a-ryang <chnwu.kim@gmail.com>
2023-10-12 11:18:35 +0900

    feat: react-query 설정 #212

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { ThemeProvider, Global, css } from "@emotion/react";
 import { IconContext } from "@phosphor-icons/react";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Suspense } from "react";
 import { HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router-dom";
@@ -8,40 +10,44 @@ import DeferredComponent from "./components/DeferredComponent";
 import Loader from "./components/Loader";
 import { AuthProvider } from "./contexts/AuthContext";
 import { SnackBarContextProvider } from "./contexts/SnackBarContext";
+import { queryClient } from "./libs/react-query";
 import router from "./routes";
 import { theme } from "./styles/theme";
 
 export default function App() {
   return (
     <HelmetProvider>
-      <ThemeProvider theme={theme}>
-        <IconContext.Provider
-          value={{
-            size: 24,
-          }}
-        >
-          <SnackBarContextProvider>
-            <Global
-              styles={css`
-                body {
-                  color: ${theme.colors.neutral["90"]};
-                }
-              `}
-            />
-            <AuthProvider>
-              <Suspense
-                fallback={
-                  <DeferredComponent>
-                    <Loader />
-                  </DeferredComponent>
-                }
-              >
-                <RouterProvider router={router} />
-              </Suspense>
-            </AuthProvider>
-          </SnackBarContextProvider>
-        </IconContext.Provider>
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools />
+        <ThemeProvider theme={theme}>
+          <IconContext.Provider
+            value={{
+              size: 24,
+            }}
+          >
+            <SnackBarContextProvider>
+              <Global
+                styles={css`
+                  body {
+                    color: ${theme.colors.neutral["90"]};
+                  }
+                `}
+              />
+              <AuthProvider>
+                <Suspense
+                  fallback={
+                    <DeferredComponent>
+                      <Loader />
+                    </DeferredComponent>
+                  }
+                >
+                  <RouterProvider router={router} />
+                </Suspense>
+              </AuthProvider>
+            </SnackBarContextProvider>
+          </IconContext.Provider>
+        </ThemeProvider>
+      </QueryClientProvider>
     </HelmetProvider>
   );
 }
