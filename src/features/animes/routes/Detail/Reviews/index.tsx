9c113bf302584ce8c8711570ab9a6922e5b51b08
a-ryang <chnwu.kim@gmail.com>
2023-10-13 14:11:03 +0900

    style 디자인 변경사항 반영 #201
    * 입덕 포인트 간격
    
    * 총 N명이 리뷰를 남겨 주셨어요 폰트 사이즈 및 하단 칩과 간격 수정
    
    * 한 줄 리뷰 위아래 border 1px로 수정

--- src/features/animes/routes/Detail/Reviews/index.tsx
+++ src/features/animes/routes/Detail/Reviews/index.tsx
@@ -1,7 +1,7 @@
 import Chip from "@/components/Chip";
 import ReviewCard from "@/features/reviews/components/ReviewCard";
 
-import { Section } from "./style";
+import { Section, TotalReviews } from "./style";
 
 const REVIEW_MOCK_DATA = {
   user: {
@@ -16,9 +16,7 @@ export default function Reviews() {
   return (
     <Section>
       <h1>한 줄 리뷰</h1>
-      <p style={{ marginTop: "8px", marginBottom: "8px" }}>
-        총 1,120명이 리뷰를 남겨 주셨어요
-      </p>
+      <TotalReviews>총 1,120명이 리뷰를 남겨 주셨어요</TotalReviews>
       <ul
         style={{
           display: "flex",
--- src/features/animes/routes/Detail/Reviews/style.ts
+++ src/features/animes/routes/Detail/Reviews/style.ts
@@ -5,3 +5,10 @@ import { SectionContainer } from "../Section.style";
 export const Section = styled(SectionContainer)`
   padding-bottom: 200px;
 `;
+
+export const TotalReviews = styled.p`
+  ${({ theme }) => theme.typo["body-3-r"]};
+  color: ${({ theme }) => theme.colors.neutral["70"]};
+  margin-top: 8px;
+  margin-bottom: 16px;
+`;
