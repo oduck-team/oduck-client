b3906f4253b1b31378f9351ef8133092d5600b60
presentKey <kim.hyeonjae4@gmail.com>
2023-10-17 00:10:46 +0900

    feat: useApi() 사용

--- src/features/users/routes/Profile/index.tsx
+++ src/features/users/routes/Profile/index.tsx
@@ -1,10 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
+
 import Head from "@/components/Head";
+import Loader from "@/components/Loader";
+import { useApi } from "@/hooks/useApi";
+import useAuth from "@/hooks/useAuth";
 
 import AboutMe from "./AboutMe";
 import { ProfileContainer } from "./style";
 import TabMenu from "./TabMenu";
 
 export default function Profile() {
+  const {
+    user: { name },
+  } = useAuth();
+  const { profile } = useApi();
+  const {
+    isLoading,
+    error,
+    data: user,
+  } = useQuery(["profile", name], () => profile.getProfile(name));
+  console.log(user);
+
+  if (isLoading) return <Loader />;
+  if (error) return <span>error</span>;
   return (
     <>
       {/* FIXME: 내 프로필 -> 내 프로필, 다른 사용자 프로필 -> 닉네임*/}
