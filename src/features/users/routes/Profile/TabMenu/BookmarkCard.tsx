5c6f4e507a912c353f09901669902428fd749b9d
presentKey <kim.hyeonjae4@gmail.com>
2023-10-22 17:14:03 +0900

    feat: 날짜 포맷 변경

--- src/features/users/routes/Profile/TabMenu/BookmarkCard.tsx
+++ src/features/users/routes/Profile/TabMenu/BookmarkCard.tsx
@@ -1,7 +1,8 @@
 import {
   BookmarkCardContainer,
-  Date,
+  CreatedDate,
   Image,
+  InfoContainer,
   MyScore,
   RatingContainer,
   Score,
@@ -18,25 +19,37 @@ export default function BookmarkCard({ bookmark }: BookmarkCardProps) {
   return (
     <BookmarkCardContainer>
       <Image src={bookmark.thumbnail} alt={bookmark.title} />
-      <div>
+      <InfoContainer>
         <Title>{bookmark.title}</Title>
-        <RatingContainer>
-          <ScoreContainer>
-            <StarIcon size={13} weight="fill" color="yellow" />
-            <Score>평균 {bookmark.avgScore / 2}</Score>
-          </ScoreContainer>
-          <ScoreContainer>
-            {bookmark.myScore >= 0 && (
-              <>
-                <StarIcon size={13} weight="fill" color="blue" />
-                <MyScore>내 평점 {bookmark.myScore / 2}</MyScore>
-              </>
-            )}
-            {bookmark.myScore < 0 && <Score>평가전</Score>}
-          </ScoreContainer>
-        </RatingContainer>
-        <Date>{bookmark.createdAt}</Date>
-      </div>
+        <div>
+          <RatingContainer>
+            <ScoreContainer>
+              <StarIcon size={13} weight="fill" color="yellow" />
+              <Score>평균 {bookmark.avgScore / 2}</Score>
+            </ScoreContainer>
+            <ScoreContainer>
+              {bookmark.myScore >= 0 && (
+                <>
+                  <StarIcon size={13} weight="fill" color="blue" />
+                  <MyScore>내 평점 {bookmark.myScore / 2}</MyScore>
+                </>
+              )}
+              {bookmark.myScore < 0 && <Score>평가전</Score>}
+            </ScoreContainer>
+          </RatingContainer>
+          <CreatedDate>{dateFormat(bookmark.createdAt)}</CreatedDate>
+        </div>
+      </InfoContainer>
     </BookmarkCardContainer>
   );
+}
+
+function dateFormat(date: string) {
+  const originalDate = new Date(date);
+
+  const year = originalDate.getFullYear();
+  const month = (originalDate.getMonth() + 1).toString().padStart(2, "0"); // 월은 0부터 시작하므로 +1 해야 함
+  const day = originalDate.getDate().toString().padStart(2, "0");
+
+  return `${year}.${month}.${day}`;
 }
