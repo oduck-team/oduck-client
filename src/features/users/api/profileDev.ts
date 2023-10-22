8ee2ee1fedbb2bed3510dff86c1c0710c0c4b4ae
presentKey <kim.hyeonjae4@gmail.com>
2023-10-22 17:14:29 +0900

    feat: 북마크 개발용 api 구현

--- src/features/users/api/profileDev.ts
+++ src/features/users/api/profileDev.ts
@@ -1,7 +1,22 @@
+import bookmarkMock1 from "./mock/bookmark1.json";
+import bookmarkMock2 from "./mock/bookmark2.json";
+import bookmarkMock3 from "./mock/bookmark3.json";
 import profileMock from "./mock/profile.json";
+import { BookmarkPage } from "./profile";
 
 export default class ProfileDevApi {
   async getProfile(): Promise<Profile> {
     return profileMock;
+  }
+
+  async getBookmark(_: number, pageParam: number): Promise<BookmarkPage> {
+    switch (pageParam) {
+      case 21:
+        return bookmarkMock2;
+      case 11:
+        return bookmarkMock1;
+      default:
+        return bookmarkMock3;
+    }
   }
 }
