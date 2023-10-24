590dd2b91b06c1922752d614d8678724c34ecd68
a-ryang <chnwu.kim@gmail.com>
2023-10-24 17:36:35 +0900

    feat: 토글 북마크, 북마크 여부 조회 API #238

---
+++ src/features/bookmarks/api/BookmarkApi.ts
@@ -0,0 +1,15 @@
+import { get, post } from "@/libs/api";
+
+export default class BookmarkApi {
+  /** @description 북마크(입덕)를 토글하기 (생성 or 삭제) */
+  async toggleBookmarK(animeId: number): Promise<void> {
+    return post("/bookmarks", {
+      animeId,
+    });
+  }
+
+  /** @description 북마크(입덕) 여부 조회 */
+  async getBookmark(animeId: number): Promise<{ createdAt: string }> {
+    return get(`/bookmarks/${animeId}`);
+  }
+}
