805b6e8389901f0cca361cb3cf6c7540ce82724b
a-ryang <chnwu.kim@gmail.com>
2023-10-24 17:42:30 +0900

    feat: 북마크 생성, 삭제를 구현한다 #238

--- src/features/bookmarks/components/BookmarkButton.tsx
+++ src/features/bookmarks/components/BookmarkButton.tsx
@@ -1,52 +1,49 @@
 import { AnimatePresence } from "framer-motion";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import Button from "@/components/Button";
 import LoginAlertModal from "@/features/auth/components/LoginAlertModal";
 import useAuth from "@/features/auth/hooks/useAuth";
 import useDebounce from "@/hooks/useDebounce";
+import { ApiError } from "@/libs/error";
 
-import useAddBookmark from "../hooks/useAddBookmark";
-// import useRemoveBookmark from "../hooks/useRemoveBookmark";
+import useBookmark from "../hooks/useBookmark";
+import useToggleBookmark from "../hooks/useToggleBookmark";
+
+const TOGGLE_DEBOUNCE = 200;
 
 interface BookmarkButtonProps {
   animeId: number;
 }
 
 export default function BookmarkButton({ animeId }: BookmarkButtonProps) {
-  const { error: addError, addBookmark } = useAddBookmark();
-  //const { error: removeError, removeBookmark } = useRemoveBookmark();
   const { isLoggedIn } = useAuth();
+  const { bookmarkedAt, fetchBookmark } = useBookmark(1);
+  const { error: toggleBookmarkError, toggleBookmark } = useToggleBookmark();
   const [isLoginModalVisible, setIsLoginModalVisible] = useState(false);
-  const isBookmarked = true; // TODO: API
+  //const { error: removeError, removeBookmark } = useRemoveBookmark();
+  const navigate = useNavigate();
 
-  const handleDebounceAddBookmark = useDebounce(async (animeId: number) => {
+  const handleToggleBookmark = useDebounce(async (animeId: number) => {
     if (!isLoggedIn) {
       setIsLoginModalVisible(true);
       return;
     }
-    try {
-      await addBookmark(animeId);
-      console.log("addBookmark");
-    } catch (e) {
-      console.log("try catch");
-      console.log(e);
-    }
-  }, 200);
 
+    await toggleBookmark(animeId);
+    await fetchBookmark(animeId); // TODO:  toast 알림 ('입덕 애니에 추가했어요' 입덕한 애니 보러가기 )
+  }, TOGGLE_DEBOUNCE);
+
+  // 북마크 요청 에러 핸들링
   useEffect(() => {
-    if (addError) {
-      switch (addError.message) {
-        case "Not Authenticate": {
-          setIsLoginModalVisible(true);
-          break;
-        }
-        case "Already bookmarked": {
-          console.log("Already bookmarked !!!");
-        }
+    if (toggleBookmarkError instanceof ApiError) {
+      if (toggleBookmarkError.status === 404) {
+        // TODO: toast 알림 '존재하지 않는 애니에요'
+        navigate("/animes");
       }
     }
-  }, [addError]);
+  }, [toggleBookmarkError, navigate]);
 
   return (
     <>
@@ -54,11 +51,11 @@ export default function BookmarkButton({ animeId }: BookmarkButtonProps) {
         name="입덕 버튼"
         size="lg"
         isBlock
-        color={isBookmarked ? "primary" : "neutral"}
+        color={bookmarkedAt ? "primary" : "neutral"}
         style={{ fontSize: "14px" }}
-        onClick={() => handleDebounceAddBookmark(animeId)}
+        onClick={() => handleToggleBookmark(animeId)}
       >
-        {isBookmarked ? "입덕한 애니" : "입덕하기"}
+        {bookmarkedAt ? "입덕한 애니" : "입덕하기"}
       </Button>
 
       <AnimatePresence>
