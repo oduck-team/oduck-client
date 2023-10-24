8031fcff504464ca7384ba1606846e80923441de
presentKey <kim.hyeonjae4@gmail.com>
2023-10-24 19:58:47 +0900

    refactor: Loader 주석 추가

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,6 +1,10 @@
 import { LoaderContainer } from "./style";
 
 export interface LoaderProps {
+  /**
+   * @desc page: (default) 페이지용 Loader
+   * @desc oduck: 작은 오덕 이미지만 나타남 (ex. cursor pagination loading)
+   */
   display?: "page" | "oduck";
 }
 
