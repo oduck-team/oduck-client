140fa7fbb63d992fa3e6dae558d7807f5108022d
a-ryang <chnwu.kim@gmail.com>
2023-10-24 17:41:33 +0900

    feat: useToggleBookmark  #238

---
+++ src/features/bookmarks/hooks/useToggleBookmark.ts
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+import { useApi } from "@/hooks/useApi";
+
+/**
+ * @description 북마크 상태를 토글(생성/삭제)합니다.
+ */
+export default function useToggleBookmark() {
+  const { bookmarkApi } = useApi();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  const toggleBookmark = async (animeId: number) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      await bookmarkApi.toggleBookmarK(animeId);
+    } catch (e) {
+      setError(e as Error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return {
+    isLoading,
+    error,
+    toggleBookmark,
+  };
+}
