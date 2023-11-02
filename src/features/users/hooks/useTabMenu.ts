87d6d980e41670884da6646e59029a68697a19ea
presentKey <kim.hyeonjae4@gmail.com>
2023-11-03 01:35:16 +0900

    feat: 프로필 Tab 메뉴 query string 적용

--- src/features/users/hooks/useTabMenu.ts
+++ src/features/users/hooks/useTabMenu.ts
@@ -1,11 +1,13 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { useRef, useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 
-// import useAuth from "@/features/auth/hooks/useAuth";
 import useSortBar from "@/features/users/hooks/useSortBar";
 import { useApi } from "@/hooks/useApi";
 import useIntersectionObserver from "@/hooks/useIntersectionObserver";
+
+const REVIEW = "review";
+export const BOOKMARK = "bookmark";
 
 export const REVIEW_MENU = "한줄리뷰";
 export const BOOKMARK_MENU = "입덕애니";
@@ -14,9 +16,15 @@ export type MENU = typeof REVIEW_MENU | typeof BOOKMARK_MENU;
 
 export default function useTabMenu(memberId: number) {
   const targetRef = useRef(null);
-  const location = useLocation();
+  const [searchParams, setSearchParams] = useSearchParams();
   const [selectedMenu, setSelectedMenu] = useState<MENU>(REVIEW_MENU);
-  const handleTabMenuClick = (text: MENU) => setSelectedMenu(text);
+  const handleTabMenuClick = (text: MENU) => {
+    setSelectedMenu(text);
+    text === "한줄리뷰"
+      ? searchParams.set("tab", REVIEW)
+      : searchParams.set("tab", BOOKMARK);
+    setSearchParams(searchParams, { replace: true });
+  };
   const {
     selected: selectedSort,
     SHEET_BUTTONS,
@@ -73,11 +81,12 @@ export default function useTabMenu(memberId: number) {
       ? bookmarks?.pages.length
       : reviews?.pages.length;
 
-  // SideBar 입덕애니 클릭 시, 입덕애니 Menu 선택
+  /** queryString tab === 'bookmark'인 경우, 입덕애니 Menu 선택 */
   useEffect(() => {
-    if (!location.state) return;
-    setSelectedMenu(BOOKMARK_MENU);
-  }, [location.state]);
+    searchParams.get("tab") === BOOKMARK
+      ? setSelectedMenu(BOOKMARK_MENU)
+      : setSelectedMenu(REVIEW_MENU);
+  }, [searchParams]);
 
   return {
     targetRef,
