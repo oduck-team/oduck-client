324269739e2c0a505b43d0ce53d14781ccb163f8
presentKey <kim.hyeonjae4@gmail.com>
2023-11-04 21:23:01 +0900

    fix: 사용자가 login url을 직접 입력한 경우 대응

--- src/routes/SavePath.tsx
+++ src/routes/SavePath.tsx
@@ -7,6 +7,12 @@ export default function SavePath() {
   useEffect(() => {
     const prevPath = sessionStorage.getItem("currPath");
 
+    /** 사용자가 login url을 직접 입력한 경우 prevPath 설정 */
+    if (location.pathname === "/login") {
+      sessionStorage.setItem("prevPath", prevPath ?? "/");
+      return;
+    }
+
     if (prevPath === "/auth/callback") sessionStorage.setItem("prevPath", "");
     else sessionStorage.setItem("prevPath", prevPath ?? "/");
 
