6daf846bbf283101114958c51ab1e4729be56546
presentKey <kim.hyeonjae4@gmail.com>
2023-11-06 22:23:54 +0900

    feat: useAnime memberId key 추가

--- src/features/animes/hooks/useAnime.ts
+++ src/features/animes/hooks/useAnime.ts
@@ -1,11 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 
+import useAuth from "@/features/auth/hooks/useAuth";
 import { useApi } from "@/hooks/useApi";
 
 export default function useAnime(animeId: number) {
   const { animeApi } = useApi();
+  const { user } = useAuth();
+
   return useQuery({
-    queryKey: ["anime", animeId],
+    queryKey: ["anime", animeId, user.memberId],
     queryFn: () => animeApi.getById(animeId),
   });
 }
