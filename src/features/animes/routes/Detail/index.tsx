275c359f2bcee0ae270a7325f2de5c3f13174d40
presentKey <kim.hyeonjae4@gmail.com>
2023-11-06 17:56:10 +0900

    cleanup: 애니 상세 페이지 error 처리 삭제

--- src/features/animes/routes/Detail/index.tsx
+++ src/features/animes/routes/Detail/index.tsx
@@ -1,11 +1,7 @@
-// import { useNavigate } from "react-router-dom";
-
 import { useParams } from "react-router-dom";
 
 import Loader from "@/components/Loader";
 import SectionDivider from "@/components/SectionDivider";
-import NotFound from "@/features/common/routes/Error/404";
-import { ApiError } from "@/libs/error";
 
 import useAnime from "../../hooks/useAnime";
 
@@ -18,24 +14,11 @@ import { AnimeDetailContainer } from "./style";
 export default function AnimeDetail() {
   const { id: animeId } = useParams();
 
-  const {
-    data: anime,
-    isLoading: isAnimeLoading,
-    error: animeError,
-  } = useAnime(Number(animeId));
-  // const navigate = useNavigate();
+  const { data: anime, isLoading: isAnimeLoading } = useAnime(Number(animeId));
 
   if (isAnimeLoading) {
     return <Loader />;
   }
-
-  if (animeError && animeError instanceof ApiError) {
-    switch (animeError.status) {
-      case 404:
-        return <NotFound />;
-    }
-  }
-
   if (anime)
     return (
       <AnimeDetailContainer>
