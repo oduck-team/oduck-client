693a10c637adc975fe43e326723d7fcc1dab7b4f
presentKey <kim.hyeonjae4@gmail.com>
2023-11-06 23:10:16 +0900

    feat: 북마크 토글 error 처리

--- src/features/bookmarks/components/BookmarkButton.tsx
+++ src/features/bookmarks/components/BookmarkButton.tsx
@@ -1,6 +1,7 @@
-import { CheckCircle } from "@phosphor-icons/react";
+import { CheckCircle, WarningCircle } from "@phosphor-icons/react";
+import { AxiosError } from "axios";
 import { AnimatePresence } from "framer-motion";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import Button from "@/components/Button";
@@ -8,7 +9,6 @@ import useToast from "@/components/Toast/useToast";
 import LoginAlertModal from "@/features/auth/components/LoginAlertModal";
 import useAuth from "@/features/auth/hooks/useAuth";
 import useDebounce from "@/hooks/useDebounce";
-import { ApiError } from "@/libs/error";
 
 import useBookmark from "../hooks/useBookmark";
 import useToggleBookmark from "../hooks/useToggleBookmark";
@@ -53,23 +53,35 @@ export default function BookmarkButton({ animeId }: BookmarkButtonProps) {
           });
         }
       },
+      onError: (error) => {
+        if (error instanceof AxiosError && error.response?.status) {
+          const status = error.response.status;
+          switch (status) {
+            case 401:
+              toast.open({
+                message: "로그인 시간이 만료되었어요.\n다시 로그인해 주세요.",
+                icon: <CheckCircle weight="fill" />,
+                iconColor: "warn",
+                buttonText: "로그인",
+                onClickButton: () => navigate("/login"),
+                position: "top",
+              });
+              break;
+            default:
+              toast.open({
+                message: "오류가 발생했어요. 잠시 후 다시 시도해 주세요.",
+                icon: <WarningCircle weight="fill" />,
+                iconColor: "warn",
+                position: "top",
+              });
+              break;
+          }
+        }
+      },
     });
   }, TOGGLE_DEBOUNCE);
 
-  // 북마크 요청 에러 핸들링
-  useEffect(() => {
-    const { error } = bookmarkMutation;
-    if (error instanceof ApiError) {
-      if (error.status === 404) {
-        // TODO: toast 알림 '존재하지 않는 애니에요'
-        navigate("/animes");
-      }
-    }
-  }, [bookmarkMutation, navigate]);
-
   const { data: bookmarkedAt } = bookmarkQuery;
-  // console.log("bookmarkedAt");
-  // console.log(bookmarkedAt);
 
   return (
     <>
