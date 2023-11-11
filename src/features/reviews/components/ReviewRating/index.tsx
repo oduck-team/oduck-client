43eeffa0f6b0cba384ef4b7c5ed333fbd011cb81
mihee <s2809h29@gmail.com>
2023-11-11 17:14:21 +0900

    feat: 별점 평가 추가, 수정 debounce 적용 #286

--- src/features/reviews/components/ReviewRating/index.tsx
+++ src/features/reviews/components/ReviewRating/index.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import Rating from "@/components/Rating";
 import LoginAlertModal from "@/features/auth/components/LoginAlertModal";
 import useAuth from "@/features/auth/hooks/useAuth";
+import useDebounce from "@/hooks/useDebounce";
 
 import useEvaluation from "../../hook/useEvaluation";
 
@@ -13,6 +14,8 @@ import { ReviewRecommend } from "./style";
 interface ReviewRatingProps {
   animeId: number;
 }
+
+const DEBOUNCE_DELAY = 200;
 
 export default function ReviewRating({ animeId }: ReviewRatingProps) {
   const hasReviewed = true; // dev용 변수
@@ -24,13 +27,14 @@ export default function ReviewRating({ animeId }: ReviewRatingProps) {
   const { data: evaluation, evaluationMutation } = useEvaluation(animeId);
   console.log("평가 여부: ", evaluation?.createdAt);
 
-  const handleRate = (value: number) => {
+  const handleRate = useDebounce((value: number) => {
     if (!isLoggedIn) {
       setIsLoginModalVisible(true);
       return;
     }
+    // 평가 추가 또는 기존과 다른 점수로 평가 수정 시에만 요청 수행
     if (evaluation?.score !== value) evaluationMutation.mutate(value);
-  };
+  }, DEBOUNCE_DELAY);
 
   return (
     <>
