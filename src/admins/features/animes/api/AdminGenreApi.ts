eacca799023c5caee47d43b9512b808b1e86ac45
a-ryang <chnwu.kim@gmail.com>
2023-11-11 15:15:07 +0900

    feat: useGenreManager #284

--- src/admins/features/animes/api/AdminGenreApi.ts
+++ src/admins/features/animes/api/AdminGenreApi.ts
@@ -1,13 +1,25 @@
 import { ADMIN_BASE_URL } from "@/admins/constants/config";
 import GenreApi from "@/features/animes/api/GenreApi";
-import { post } from "@/libs/api";
+import { del, patch, post } from "@/libs/api";
 
 interface CreateGenreDto {
+  name: string;
+}
+
+interface UpdateGenreDto {
   name: string;
 }
 
 export default class AdminGenreApi extends GenreApi {
   create(dto: CreateGenreDto) {
     return post(ADMIN_BASE_URL + "/genres", dto);
+  }
+
+  update(id: number, dto: UpdateGenreDto) {
+    return patch(ADMIN_BASE_URL + `/genres/${id}`, dto);
+  }
+
+  delete(id: number) {
+    return del(ADMIN_BASE_URL + `/genres/${id}`);
   }
 }
