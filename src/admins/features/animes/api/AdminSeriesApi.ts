7393b4997b20a78a8d39a34e6ae22eaa1f9f191b
a-ryang <chnwu.kim@gmail.com>
2023-11-11 15:16:35 +0900

    feat: useSeriesManager #284

--- src/admins/features/animes/api/AdminSeriesApi.ts
+++ src/admins/features/animes/api/AdminSeriesApi.ts
@@ -1,16 +1,28 @@
 import { ADMIN_BASE_URL } from "@/admins/constants/config";
-import { get, post } from "@/libs/api";
+import { del, get, patch, post } from "@/libs/api";
 
 interface CreateSeriesDto {
   title: string;
 }
 
+interface UpdateSeriesDto {
+  title: string;
+}
+
 export default class AdminSeriesApi {
+  create(dto: CreateSeriesDto) {
+    return post(ADMIN_BASE_URL + "/series", dto);
+  }
+
   getList() {
     return get<Series[]>(ADMIN_BASE_URL + "/series");
   }
 
-  create(dto: CreateSeriesDto) {
-    return post(ADMIN_BASE_URL + "/series", dto);
+  update(id: number, dto: UpdateSeriesDto) {
+    return patch(ADMIN_BASE_URL + `/series/${id}`, dto);
+  }
+
+  delete(id: number) {
+    return del(ADMIN_BASE_URL + `/series/${id}`);
   }
 }
