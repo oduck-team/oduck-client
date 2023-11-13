948c37d96284c96c38122c3cebbc0e743104c373
a-ryang <chnwu.kim@gmail.com>
2023-11-13 16:32:23 +0900

    chore: 페이지별 Head 추가

--- src/features/common/routes/Error/404/index.tsx
+++ src/features/common/routes/Error/404/index.tsx
@@ -1,3 +1,5 @@
+import Head from "@/components/Head";
+
 import {
   NotFoundContainer,
   Content,
@@ -8,18 +10,21 @@ import {
 
 export default function NotFound() {
   return (
-    <NotFoundContainer>
-      <Content>
-        <div>
-          <ErrorImage src="/logo/logo-empty.png" alt="error" />
-        </div>
-        <Message>페이지를 찾을 수 없어요</Message>
-        <div>
-          <HomeButton size="lg" to="/" reloadDocument>
-            홈으로
-          </HomeButton>
-        </div>
-      </Content>
-    </NotFoundContainer>
+    <>
+      <Head title="오덕 | 404" />
+      <NotFoundContainer>
+        <Content>
+          <div>
+            <ErrorImage src="/logo/logo-empty.png" alt="error" />
+          </div>
+          <Message>페이지를 찾을 수 없어요</Message>
+          <div>
+            <HomeButton size="lg" to="/" reloadDocument>
+              홈으로
+            </HomeButton>
+          </div>
+        </Content>
+      </NotFoundContainer>
+    </>
   );
 }
