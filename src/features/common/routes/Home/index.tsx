e6f8de5d5bb6d782bb8072d430a32e05f00c4fc4
presentKey <kim.hyeonjae4@gmail.com>
2023-11-13 21:36:23 +0900

    feat: RecommendAnimes 컴포넌트 구현 및 getRecommendList api 적용

--- src/features/common/routes/Home/index.tsx
+++ src/features/common/routes/Home/index.tsx
@@ -4,47 +4,16 @@ import { useNavigate } from "react-router-dom";
 import Button from "@/components/Button";
 import AnimeCarousel from "@/features/animes/components/AnimeCarousel";
 import AnimeRanking from "@/features/animes/components/AnimeRanking";
-import AnimeSlide from "@/features/animes/components/AnimeSlide";
 import useAuth from "@/features/auth/hooks/useAuth";
 
 import Discord from "./Discord";
 import NewestAnimes from "./NewestAnimes";
 import RecentReview from "./RecentReview";
+import RecommendAnimes from "./RecommendAnimes";
 
 export default function Home() {
-  const cardAnime = {
-    id: 1,
-    title: "주술회전",
-    thumbnail: "https://url.kr/lo4miy",
-    starScoreAvg: 5.0,
-  };
-
-  const cardAnime2 = {
-    id: 2,
-    title:
-      "레벨 1이지만 유니크 스킬로 최강이 되었습니다 레벨 1이지만 유니크 스킬로 최강이 되었습니다",
-    thumbnail: "https://url.kr/azbxi1",
-    starScoreAvg: 5.0,
-  };
-
-  const slideAnimes = [
-    cardAnime,
-    cardAnime2,
-    cardAnime,
-    cardAnime2,
-    cardAnime,
-    cardAnime2,
-    cardAnime,
-    cardAnime2,
-    cardAnime,
-    cardAnime2,
-    cardAnime,
-    cardAnime2,
-  ];
-
   const { user } = useAuth();
   const navigate = useNavigate();
-
   const handlerReviewButtonClick = () => {
     if (user) navigate("/search");
     else navigate("/login");
@@ -57,8 +26,9 @@ export default function Home() {
       <Discord />
       <NewestAnimes />
       <RecentReview />
-      <AnimeSlide title="덕후들의 눈물샘을 터뜨린" animes={slideAnimes} />
-      <AnimeSlide title="이불밖을 못 나오게 하는" animes={slideAnimes} />
+      {/* TODO: 추천 애니 요청 방법 정하기 */}
+      <RecommendAnimes title="덕후들의 눈물샘을 터뜨린" />
+      <RecommendAnimes title="이불밖을 못 나오게 하는" />
       <Bottom>
         <span>감명 깊게 본 애니를 다른 회원님들과 공유해보세요!</span>
         <Button name="리뷰" size="lg" onClick={handlerReviewButtonClick}>
