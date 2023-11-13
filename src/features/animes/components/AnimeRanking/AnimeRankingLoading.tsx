0e3e006dcd3a9d9c13a6c1e4d973f087310c2b9a
presentKey <kim.hyeonjae4@gmail.com>
2023-11-13 20:02:07 +0900

    style: AnimeRankingLoading 스타일 변경

--- src/features/animes/components/AnimeRanking/AnimeRankingLoading.tsx
+++ src/features/animes/components/AnimeRanking/AnimeRankingLoading.tsx
@@ -5,9 +5,9 @@ import Skeleton from "@/components/Skeleton";
 export default function AnimeRankingLoading() {
   return (
     <AnimeRankingLoadingContainer>
-      <Skeleton w={"full"} h={276} borderRadius={5} />
+      <Skeleton w={"full"} h={158} borderRadius={5} />
       <SliderContainer>
-        {Array.from({ length: 5 }, (_, index) => (
+        {Array.from({ length: 7 }, (_, index) => (
           <ItemContainer key={index}>
             <Skeleton w={95} h={95} borderRadius={5} />
             <Skeleton w={95} h={36} />
@@ -27,6 +27,14 @@ const SliderContainer = styled.div`
   display: flex;
   gap: 16px;
   margin-top: 16px;
+  margin-right: -16px;
+  overflow-x: auto;
+  -ms-overflow-style: none; /* 익스플로러, 앳지 */
+  scrollbar-width: none; /* 파이어폭스 */
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
 const ItemContainer = styled.div`
