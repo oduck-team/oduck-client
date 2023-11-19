6b5e9ef3542af5e5e49d6de5a0db01f64d723f08
presentKey <kim.hyeonjae4@gmail.com>
2023-11-20 04:00:56 +0900

    feat: MainCarousel autoplay 설정

--- src/libs/carousel/index.ts
+++ src/libs/carousel/index.ts
@@ -1,10 +1,14 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+/** @link https://github.com/akiran/react-slick */
+
 export const MainCarousel = {
   dots: true,
   infinite: true,
   speed: 500,
+  autoplay: true,
+  autoplaySpeed: 5000,
   slidesToShow: 1,
   slidesToScroll: 1,
 };
