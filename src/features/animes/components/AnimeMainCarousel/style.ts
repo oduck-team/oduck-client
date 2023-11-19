005ff50f7ca5a311e660b587f49b15a843a26f4a
presentKey <kim.hyeonjae4@gmail.com>
2023-11-20 03:57:34 +0900

    style: carousel dots 스타일 overwrite

--- src/features/animes/components/AnimeMainCarousel/style.ts
+++ src/features/animes/components/AnimeMainCarousel/style.ts
@@ -7,6 +7,35 @@ export const AnimeMainCarouselContainer = styled.div<{ image: string }>`
   height: 545px;
   overflow: hidden;
 
+  & .slick-dots {
+    position: absolute;
+    bottom: 40px;
+    left: 0;
+
+    li {
+      margin: 0 2px;
+    }
+
+    li,
+    button {
+      width: 10px;
+    }
+
+    .slick-active {
+      button::before {
+        color: ${({ theme }) => theme.colors.neutral["05"]};
+        opacity: 1;
+        width: 10px;
+      }
+    }
+
+    button::before {
+      color: ${({ theme }) => theme.colors.neutral[50]};
+      opacity: 1;
+      width: 10px;
+    }
+  }
+
   /** 배경 blur 처리 */
   &::before {
     content: "";
@@ -16,12 +45,11 @@ export const AnimeMainCarouselContainer = styled.div<{ image: string }>`
     top: 0;
     left: 0;
     ${({ image }) => css`
-    background:
-      linear-gradient(0deg, rgba(0, 0, 0, 0.5) 50%, rgba(0, 0, 0, 0.5) 100%),
-      url(${image}),
-      lightgray -36.515px 1.434px / 119.018% 99.743% no-repeat;
-      background-
-  `}
+      background:
+        linear-gradient(0deg, rgba(0, 0, 0, 0.5) 50%, rgba(0, 0, 0, 0.5) 100%),
+        url(${image}),
+        lightgray -36.515px 1.434px / 119.018% 99.743% no-repeat;
+    `}
     background-size: cover;
     background-position: center;
     filter: blur(5px);
