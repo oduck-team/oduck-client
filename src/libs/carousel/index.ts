8db62fd932ff7c0b2d6f07cfe3f7913ff9264909
presentKey <kim.hyeonjae4@gmail.com>
2023-11-20 02:41:41 +0900

    feat: Carousel lib 설정 파일

---
+++ src/libs/carousel/index.ts
@@ -0,0 +1,10 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+export const MainCarousel = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+};
