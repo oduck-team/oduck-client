e77704bc21e31eb82e2abe766526a2a0ce0bdd51
mihee <s2809h29@gmail.com>
2023-11-20 21:11:17 +0900

    feat: 평가 추가/수정 시 리뷰 목록 조회 query 무효화
    - 사용자가 해당 애니의 리뷰를 작성한 경우에만 적용

--- src/features/reviews/hook/useEvaluation.ts
+++ src/features/reviews/hook/useEvaluation.ts
@@ -5,7 +5,13 @@ import useAuth from "@/features/auth/hooks/useAuth";
 import { useApi } from "@/hooks/useApi";
 import { useCommonToastError } from "@/libs/error";
 
-export default function useEvaluation(animeId: number) {
+export default function useEvaluation({
+  animeId,
+  hasReview = true,
+}: {
+  animeId: number;
+  hasReview?: boolean;
+}) {
   const queryClient = useQueryClient();
   const { reviewApi } = useApi();
   const { user } = useAuth();
@@ -28,6 +34,14 @@ export default function useEvaluation(animeId: number) {
       queryClient.invalidateQueries(["evaluation", animeId, user?.memberId]);
       // 애니 평균 평점 조회 query 무효화
       queryClient.invalidateQueries(["averageRating", animeId, user?.memberId]);
+
+      // 별점 수정 대상 애니에 대한 사용자의 리뷰가 존재하는 경우에만 리뷰 목록 조회 무효화
+      if (hasReview) {
+        queryClient.invalidateQueries(["profile", user?.memberId, "review"]);
+        queryClient.invalidateQueries(["review", animeId, user?.memberId]);
+        queryClient.invalidateQueries(["anime", animeId, user?.memberId]);
+        // TODO: 최신 리뷰 목록 조회 무효화
+      }
     },
     onError: (error) => {
       if (error instanceof AxiosError && error.response?.status) {
