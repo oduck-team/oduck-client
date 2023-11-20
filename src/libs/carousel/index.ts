1bcbbf6d98c69cec60f901c92b40839d91631db2
presentKey <kim.hyeonjae4@gmail.com>
2023-11-21 04:37:01 +0900

    feat: scroll 세팅 변경

--- src/libs/carousel/index.ts
+++ src/libs/carousel/index.ts
@@ -26,23 +26,23 @@ export const SyncingMainCarousel = {
 
 export const SyncingSubCarousel = {
   slidesToShow: 5,
-  slidesToScroll: 1,
+  slidesToScroll: 5,
   focusOnSelect: true,
-  swipeToSlide: true,
-  touchThreshold: 300,
+  speed: 100,
+  touchThreshold: 1000,
   responsive: [
     {
       breakpoint: 576,
       settings: {
         slidesToShow: 3,
-        slidesToScroll: 1,
+        slidesToScroll: 3,
       },
     },
     {
       breakpoint: 768,
       settings: {
         slidesToShow: 4,
-        slidesToScroll: 1,
+        slidesToScroll: 4,
       },
     },
   ],
