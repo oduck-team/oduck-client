97883caaf796c587d5424c67fbbe024c1777f1eb
presentKey <kim.hyeonjae4@gmail.com>
2023-11-27 17:09:01 +0900

    fix: 새로고침 수정 테스트

--- src/routes/privateRoutes.tsx
+++ src/routes/privateRoutes.tsx
@@ -3,6 +3,7 @@ import { RouteObject, useNavigate } from "react-router-dom";
 
 import Layout from "@/components/Layout";
 import useAuth from "@/features/auth/hooks/useAuth";
+import useRedirect from "@/hooks/useRedirect";
 
 import RouteLayout from "../components/Layout/RouteLayout";
 
@@ -10,8 +11,9 @@ const Profile = lazy(() => import("@/features/users/routes/Profile"));
 const ProfileEdit = lazy(() => import("@/features/users/routes/Edit"));
 
 function PrivateRoute({ children }: PropsWithChildren) {
-  const { user } = useAuth();
+  const { user, fetchUser } = useAuth();
   const navigate = useNavigate();
+  const { redirectUrl, handleRedirect } = useRedirect();
 
   /**
    * 유저 정보가 없다면 로그인 페이지로 이동합니다.
@@ -21,10 +23,19 @@ function PrivateRoute({ children }: PropsWithChildren) {
     console.log(user);
 
     if (!user) {
+      if (redirectUrl) {
+        const handleFetchUser = async () => {
+          console.log("PrivateRoute redirect handleFetchUser");
+          await fetchUser();
+          handleRedirect();
+        };
+
+        handleFetchUser();
+      }
       console.log("PrivateRoute user가 없어서 로그인 페이지로 이동");
       navigate("/login", { replace: true });
     }
-  }, [navigate, user]);
+  }, [navigate, user, redirectUrl]);
 
   return children;
 }
