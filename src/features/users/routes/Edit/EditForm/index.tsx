0cc5b2a377afa189c34cc2ceabd98a12d9815acd
presentKey <kim.hyeonjae4@gmail.com>
2023-12-06 01:12:36 +0900

    feat: 자기소개 textarea height 설정

--- src/features/users/routes/Edit/EditForm/index.tsx
+++ src/features/users/routes/Edit/EditForm/index.tsx
@@ -40,6 +40,7 @@ export default function EditForm({ name, description }: EditFromProps) {
             value={form.description}
             placeholder="자기소개를 적어보세요(최대 100자까지 가능합니다)"
             maxLength={100}
+            height={"100"}
             spellCheck={false}
             onChange={(e) => handleInputChange(e)}
           />
