b4c8c9d23c4675d266ddde30c0d44f6715a1189a
presentKey <kim.hyeonjae4@gmail.com>
2023-12-11 22:35:42 +0900

    cleanup: reviewDev.ts deprecated

--- src/contexts/OduckApiContext.tsx
+++ src/contexts/OduckApiContext.tsx
@@ -6,7 +6,6 @@ import AuthApi from "@/features/auth/api/AuthApi";
 import BookmarkApi from "@/features/bookmarks/api/BookmarkApi";
 import FileApi from "@/features/files/api/FileApi";
 import ReviewApi from "@/features/reviews/api/review";
-import ReviewDevApi from "@/features/reviews/api/reviewDev";
 import ProfileApi from "@/features/users/api/profile";
 import { StrictPropsWithChildren } from "@/types";
 
@@ -17,19 +16,18 @@ interface API {
   genreApi: GenreApi;
   bookmarkApi: BookmarkApi;
   fileApi: FileApi;
-  reviewApi: ReviewApi | ReviewDevApi;
+  reviewApi: ReviewApi;
 }
 
 export const OduckApiContext = createContext<API | null>(null);
 
-/** @desc: 서버 Api와 개발용 Api 선택 */
+/** @desc: api */
 const authApi = new AuthApi();
 const profile = new ProfileApi();
 const animeApi = new AnimeApi();
 const genreApi = new GenreApi();
 const bookmarkApi = new BookmarkApi();
 const fileApi = new FileApi();
-// const reviewApi = new ReviewDevApi();
 const reviewApi = new ReviewApi();
 
 export function OduckApiProvider({ children }: StrictPropsWithChildren) {
