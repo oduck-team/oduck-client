ba9e06d653be14fb781aa9f22b0ed7e6881700f7
mihee <s2809h29@gmail.com>
2023-12-17 23:22:34 +0900

    fix: 리뷰 좋아요 여부 query enabled 조건 수정 #270

--- src/features/reviews/hook/useGetIsLike.ts
+++ src/features/reviews/hook/useGetIsLike.ts
@@ -4,15 +4,22 @@ import { useLocation } from "react-router-dom";
 import useAuth from "@/features/auth/hooks/useAuth";
 import { useApi } from "@/hooks/useApi";
 
-export default function useGetIsLiked(reviewId: number) {
+/**
+ *
+ * @description 사용자의 리뷰 좋아요 여부 조회
+ * 리뷰 목록 조회에서 사용됩니다.
+ */
+export default function useGetIsLike(reviewId: number) {
   const { reviewApi } = useApi();
   const { user } = useAuth();
   const { pathname } = useLocation();
 
   const { data } = useQuery({
     queryKey: ["reviewLike", reviewId, user?.memberId],
-    queryFn: () => reviewApi.getReviewIsLiked(reviewId),
-    enabled: Boolean(user?.memberId) && pathname.split("/")[1] === "animes",
+    queryFn: () => reviewApi.getReviewIsLike(reviewId),
+    enabled:
+      Boolean(user?.memberId) &&
+      ["profile", "animes", "reviews"].includes(pathname.split("/")[1]),
   });
 
   return data;
