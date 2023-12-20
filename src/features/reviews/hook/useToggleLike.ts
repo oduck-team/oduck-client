e8c9013e3fa59564b2215a44ceec762f523b9fc0
mihee <s2809h29@gmail.com>
2023-12-20 17:51:49 +0900

    refactor: useToggleLike 주석 추가 #270

--- src/features/reviews/hook/useToggleLike.ts
+++ src/features/reviews/hook/useToggleLike.ts
@@ -8,11 +8,10 @@ import {
 import { AxiosError } from "axios";
 
 import useAuth from "@/features/auth/hooks/useAuth";
+import { ReviewInfo } from "@/features/reviews/api/review";
 import { ReviewListResponse } from "@/features/users/api/profile";
 import { useApi } from "@/hooks/useApi";
 import { useCommonToastError } from "@/libs/error";
-
-import { ReviewInfo } from "../api/review";
 
 export default function useToggleLike(
   reviewId: number,
@@ -39,25 +38,25 @@ export default function useToggleLike(
         queryClient,
         reviewId,
         isLike,
-        ["review", animeId, user?.memberId],
+        ["review", animeId, user?.memberId], // 애니 리뷰 목록
       );
       const prevUserReviewList = optimisticUpdateReviews(
         queryClient,
         reviewId,
         isLike,
-        ["profile", user?.memberId, "review"],
+        ["profile", user?.memberId, "review"], // 회원 리뷰 목록
       );
       const prevRecentReviewList = optimisticUpdateReviews(
         queryClient,
         reviewId,
         isLike,
-        ["MostRecentReviewList", "list"],
+        ["MostRecentReviewList", "list"], // 최신 리뷰 목록
       );
       const prevMostRecentReview = optimisticUpdateReviews(
         queryClient,
         reviewId,
         isLike,
-        ["MostRecentReviewList", "first"],
+        ["MostRecentReviewList", "first"], // 가장 최신 리뷰 (1개)
       );
 
       const prevLike = { isLike };
@@ -74,9 +73,10 @@ export default function useToggleLike(
       };
     },
     onSuccess: () => {
+      // 좋아요 요청 성공 시 리뷰 목록 query 무효화
       queryClient.invalidateQueries({
         queryKey: ["review", animeId, user?.memberId],
-        refetchType: "none",
+        refetchType: "none", // 사용자의 요청 전까지는 refetch가 일어나지 않도록 합니다.
       });
       queryClient.invalidateQueries({
         queryKey: ["profile", user?.memberId, "review"],
@@ -96,6 +96,7 @@ export default function useToggleLike(
       if (!context) return;
 
       // roll back
+      // 바꾸었던 query data를 이전으로 되돌립니다.
       queryClient.setQueryData(
         ["reviewLike", reviewId, user?.memberId],
         context.prevLike,
@@ -137,6 +138,11 @@ export default function useToggleLike(
   });
 }
 
+/**
+ * @description 리뷰 목록을 낙관적으로 업데이트하기 위한 함수
+ * @param queryKey 업데이트 할 리뷰 목록 query의 key
+ * @returns 기존 query 데이터를 반환합니다. 이는 좋아요 요청 실패 시 roll back에서 사용됩니다.
+ */
 function optimisticUpdateReviews(
   queryClient: QueryClient,
   reviewId: number,
@@ -174,6 +180,11 @@ function optimisticUpdateReviews(
   return prevReviewList;
 }
 
+/**
+ * @description 리뷰 목록 낙관적 업데이트 roll back 함수 : prevList 값으로 query 데이터를 되돌립니다.
+ * @param queryKey 되돌릴 리뷰 목록 query의 key
+ * @param prevList 기존 리뷰 목록 query 데이터
+ */
 function rollBack(
   queryClient: QueryClient,
   queryKey: QueryKey,
