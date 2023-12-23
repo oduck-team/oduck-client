406145e3f9b9dd96e54ad837599db5b87931fb0e
presentKey <kim.hyeonjae4@gmail.com>
2023-12-23 19:08:53 +0900

    feat: 이미지 확장자 검사 구현

--- src/features/users/hooks/useCropModal.tsx
+++ src/features/users/hooks/useCropModal.tsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
 
+import useToast from "@/components/Toast/useToast";
 import { readFile } from "@/libs/imageCrop";
+
+const allowedExtensions = ["jpg", "jpeg", "png", "webp"];
 
 export default function useCropModal(
   inputRef: React.RefObject<HTMLInputElement>,
 ) {
   const [isImageCropModal, setIsImageCropModal] = useState(false);
   const [imageSrc, setImageSrc] = useState<string | null>(null); // 사용자의 원본 이미지
+  const toast = useToast();
 
   const handleImageEditClick = () => {
     setIsImageCropModal(true);
@@ -18,9 +22,21 @@ export default function useCropModal(
     resetUploadedImage();
   };
 
+  /** input file change */
   const handleImageChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files.length > 0) {
       const file = e.target.files[0];
+      const fileExtension = file.name.split(".").at(-1)?.toLowerCase();
+
+      if (!allowedExtensions.includes(fileExtension ?? "")) {
+        toast.error({
+          message: `'jpg' 'jpeg' 'png' 'webp' 확장자 이미지를 등록해주세요.`,
+          duration: 4,
+        });
+        e.target.value = "";
+        return;
+      }
+
       const imageDataUrl = await readFile(file);
       setImageSrc(imageDataUrl);
     }
