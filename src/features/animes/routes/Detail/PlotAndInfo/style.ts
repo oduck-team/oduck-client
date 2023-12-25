c2f9b2378e4b54b126dedd96f0bf604e6a44ed8c
mihee <s2809h29@gmail.com>
2023-12-25 22:43:54 +0900

    style: 애니 상세페이지 스타일 변경사항 적용

--- src/features/animes/routes/Detail/PlotAndInfo/style.ts
+++ src/features/animes/routes/Detail/PlotAndInfo/style.ts
@@ -1,13 +1,9 @@
 import styled from "@emotion/styled";
 
-export const Plot = styled.p<{ isExpanded: boolean }>`
+export const Plot = styled.p`
   display: -webkit-box;
   margin-top: 8px;
-  margin-bottom: 8px;
-  -webkit-line-clamp: ${({ isExpanded }) => (isExpanded ? "none" : "2")};
-  -webkit-box-orient: vertical;
-  overflow: hidden;
-  text-overflow: ellipsis;
+  ${({ theme }) => theme.typo["body-3-r"]};
 `;
 
 export const Grid = styled.ul`
@@ -23,12 +19,15 @@ export const Grid = styled.ul`
   }
 
   & > li > span:first-of-type {
-    color: ${({ theme }) => theme.colors.neutral["70"]};
+    color: ${({ theme }) => theme.colors.neutral["60"]};
     padding-right: 8px;
     max-width: 140px;
+    ${({ theme }) => theme.typo["body-3-r"]};
+    letter-spacing: normal;
   }
 
   & > li > span:last-of-type {
     text-align: left;
+    ${({ theme }) => theme.typo["body-3-m"]};
   }
 `;
